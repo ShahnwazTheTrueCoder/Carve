2307bec64e6626d7681b663ac195b957a0193870
Saif Alam <saifalam8993@gmail.com>
2024-08-12 23:35:32 +0530

    Header and main user interface changes, including making the design responsive.
    This version clearly conveys that both the header and the main user interface are undergoing changes with an emphasis on making the design responsive.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,17 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HeaderComponent } from './header.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      declarations: [HomeComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
