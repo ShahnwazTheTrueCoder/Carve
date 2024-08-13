196d145151e338be82e4c944fde84ab2b551b369
Saif Alam <saifalam8993@gmail.com>
2024-08-13 16:04:33 +0530

    new section insert and make responsive user interface

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatIconModule,MatButtonModule],
+  imports: [MatIconModule,MatButtonModule,MatCardModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
