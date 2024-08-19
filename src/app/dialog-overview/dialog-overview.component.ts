b387c64b572f26b4f2956e48e91e0d366800766f
sam <sam7051@gmail.com>
2024-08-19 18:36:52 +0530

    added new dailog...

--- src/app/dialog-overview/dialog-overview.component.ts
+++ src/app/dialog-overview/dialog-overview.component.ts
@@ -7,18 +7,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-overview',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule,CommonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule,CommonModule,RouterModule],
   templateUrl: './dialog-overview.component.html',
   styleUrl: './dialog-overview.component.scss'
 })
 export class DialogOverviewComponent {
   form: FormGroup;
   createRoomDailog = false
-  constructor(public dialogRef: MatDialogRef<DialogOverviewComponent>, private fb: FormBuilder,) {
+  constructor(public dialogRef: MatDialogRef<DialogOverviewComponent>, private fb: FormBuilder,
+    public router:Router
+  ) {
 
     this.form = this.fb.group({
       roomCode: ['', [Validators.required, Validators.pattern('^[0-9]*$')]]
@@ -34,9 +37,10 @@ onClose(): void {
 }
 
 joinRoom(): void {
+  this.router.navigateByUrl('/chat-room')
+  this.dialogRef.close();
   if (this.form.valid) {
     console.log('Join Room clicked with code:', this.form.value.roomCode);
-    this.dialogRef.close();
   }
 }
 
