37e4ee5cd94a5cd1cd1f9041ec114921c45ce030
Saif Alam <saifalam8993@gmail.com>
2024-08-20 18:55:51 +0530

    mplement the footer and enhance the user interface. Make changes to the main component's user interface and insert art on the landing page.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,MatDividerModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
