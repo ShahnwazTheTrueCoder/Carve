002d74357b2d7a822a9b7fb2e000f429f6b4b4b3
sam <sam7051@gmail.com>
2024-09-07 16:54:53 +0530

    try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, OnInit, Inject, PLATFORM_ID} from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+// import { CommonModule, isPlatformBrowser } from '@angular/common';
+// import { Component, OnInit, Inject, PLATFORM_ID} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,13 +13,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'Messaging-app';
-  constructor(@Inject(PLATFORM_ID) private platformId: any){}
+  // constructor(@Inject(PLATFORM_ID) private platformId: any){}
   ngOnInit(){
-    if(isPlatformBrowser(this.platformId)){
-      if(localStorage.getItem("user") == null){
-        localStorage.setItem("user",window.prompt("Enter User Name"))
-      }
-    }
+    // if(isPlatformBrowser(this.platformId)){
+    //   if(localStorage.getItem("user") == null){
+    //     localStorage.setItem("user",window.prompt("Enter User Name"))
+    //   }
+    // }
   }
 }
 // localStorage.getItem('userName');
